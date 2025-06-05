d9e0c583713f7db88292cccba1561f73c3cdd7fe
Ajlamas <104563295+Ajlamas@users.noreply.github.com>
2025-06-05 12:35:42 +0200

    Update estadisticas.js

--- financiero/banca/estadisticas.js
+++ financiero/banca/estadisticas.js
@@ -16,15 +16,15 @@ router.get("/estadisticas", (req, res) => {
       <text x="47" y="120" font-size="18" font-weight="lighter" fill="#fff">Aceptados</text>
       <rect width="171" height="112.5" x="193.25" y="30.75" fill="#071A2D" rx="11.25"/>
       <rect width="171" height="112.5" x="193.25" y="30.75" stroke="#2A4056" stroke-width="1.5" rx="11.25"/>
-      <text x="212" y="80" font-size="42" font-weight="bolder" fill="#FF575A">25</text>
+      <text x="212" y="80" font-size="42" font-weight="bolder" fill="#FF575A">${rechazados}</text>
       <text x="229" y="120" font-size="18" font-weight="lighter" fill="#fff">Rechazados</text>
       <rect width="171" height="112.5" x="10.75" y="154.75" fill="#071A2D" rx="11.25"/>
       <rect width="171" height="112.5" x="10.75" y="154.75" stroke="#2A4056" stroke-width="1.5" rx="11.25"/>
-      <text x="30" y="200" font-size="42" font-weight="bolder" fill="#2ABEDF">25</text>
+      <text x="30" y="200" font-size="42" font-weight="bolder" fill="#2ABEDF">${agendados}</text>
       <text x="47" y="244" font-size="18" font-weight="lighter" fill="#fff">Agendados</text>
       <rect width="171" height="112.5" x="193.25" y="154.75" fill="#061729" rx="11.25"/>
       <rect width="171" height="112.5" x="193.25" y="154.75" stroke="#2A4056" stroke-width="1.5" rx="11.25"/>
-      <text x="212" y="200" font-size="42" font-weight="bolder" fill="#FFB109">25</text>
+      <text x="212" y="200" font-size="42" font-weight="bolder" fill="#FFB109">${pendientes}</text>
       <text x="229" y="244" font-size="18" font-weight="lighter" fill="#fff">Pendientes</text>
     </svg>
   `);
