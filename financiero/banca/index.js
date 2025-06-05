9399aab58ecb3de4c8527a49a1cd09837a43c0de
Ajlamas <104563295+Ajlamas@users.noreply.github.com>
2025-06-05 16:07:32 +0200

    Update index.js

--- financiero/banca/index.js
+++ financiero/banca/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const estadisticasRoute = require("./estadisticas");
 const enlaceRoute = require("./enlace");
 const generadoRoute = require("./generado");
+const recompensaceroRoute = require("./recompensacero");
 
 router.use("/estadisticas", estadisticasRoute); // /svg/financiero/banca/estadisticas
 router.use("/enlace", enlaceRoute);             // /svg/financiero/banca/enlace
 router.use("/generado", generadoRoute);             // /svg/financiero/banca/enlace
+router.use("/recompensacero", recompensaceroRoute);             // /svg/financiero/banca/enlace
 
 module.exports = router;
