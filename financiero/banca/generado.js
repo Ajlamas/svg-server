0dab93b3f535dccd3229c6f4d749e543aac1563e
Ajlamas <104563295+Ajlamas@users.noreply.github.com>
2025-06-05 15:05:47 +0200

    Update generado.js

--- financiero/banca/generado.js
+++ financiero/banca/generado.js
@@ -12,8 +12,8 @@ router.get("/", (req, res) => {
   <ellipse cx="187" rx="300" ry="124" fill="#CFA15C"/>
   <ellipse cx="187" cy="45" rx="300" ry="75.2" fill="#fff"/>
   <path stroke="#A1A5A8" stroke-dasharray="5 5" stroke-width=".5" d="M0 1 H375"/>
-  <text x="50%" y="38" font-size="25" font-weight="lighter" fill="#7E7F80" anchor="middle">Saldo Acumulado:</text>
-  <text x="50%" y="95" font-size="56" font-weight="bolder" fill="#071A2D" anchor="middle">${generado} 
+  <text x="50%" y="38" font-size="25" font-weight="lighter" fill="#7E7F80" text-anchor="middle">Saldo Acumulado:</text>
+  <text x="50%" y="95" font-size="56" font-weight="bolder" fill="#071A2D" text-anchor="middle">${generado} 
       <tspan fill="#CFA15C" font-size="26" font-weight="lighter">€</tspan></text>
 </svg>
   `);
