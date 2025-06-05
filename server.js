deb39ce5618f4fd04682c626fa72ff6d58925c0c
Ajlamas <104563295+Ajlamas@users.noreply.github.com>
2025-06-05 11:20:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Importa la ruta del SVG
+// Importa la ruta desde financiero/banca/estadisticas.js
 const estadisticasRoute = require("./financiero/banca/estadisticas");
-app.use("svg/financiero/banca", estadisticasRoute);
+
+// Monta la ruta bajo /svg/financiero/banca
+app.use("/svg/financiero/banca", estadisticasRoute);
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en puerto ${PORT}`);
