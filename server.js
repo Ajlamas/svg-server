cfd5b555806777a8b1a85b05e43949ef67f88f5e
Ajlamas <104563295+Ajlamas@users.noreply.github.com>
2025-06-05 11:11:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 // Importa la ruta del SVG
 const estadisticasRoute = require("./financiero/banca/estadisticas");
-app.use("/financiero", estadisticasRoute);
+app.use("/financiero/banca", estadisticasRoute);
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en puerto ${PORT}`);
