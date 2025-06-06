1452e5ebccbdf100051a21a0f59518cf1dde8592
Ajlamas <104563295+Ajlamas@users.noreply.github.com>
2025-06-06 15:44:39 +0200

    Update index.js

--- financiero/citas/index.js
+++ financiero/citas/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const citas = require("./citas");
 
 const cabecera = require("./cabecera");
 router.use("/cabecera", cabecera);
+router.use("/citas", citas);
 
 module.exports = router;
 
