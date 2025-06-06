6c9a78ac23ba74e31d9ff42938d27ae8c5f305e1
Ajlamas <104563295+Ajlamas@users.noreply.github.com>
2025-06-06 18:44:29 +0200

    Update index.js

--- financiero/citas/index.js
+++ financiero/citas/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
+
 const citas = require("./citas");
 const boton-calendario = require("./boton-calendario");
-
 const cabecera = require("./cabecera");
+
 router.use("/cabecera", cabecera);
 router.use("/citas", citas);
-router.use(/boton-calendario.js", boton-calendario);
+router.use("/boton-calendario", boton-calendario);
 
 module.exports = router;
 
