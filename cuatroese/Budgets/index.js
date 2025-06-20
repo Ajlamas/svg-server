78dd69e9f4e13f2c9ae556aba03c527307ec6fe8
Ajlamas <alberto.l.lage@gmail.com>
2025-06-20 19:54:52 +0200

    ajustar dirección index budgets cuatroese

--- cuatroese/Budgets/index.js
+++ cuatroese/Budgets/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 // Asegúrate de que el nombre aquí está en minúsculas
-const componentebudgetRoute = require("./componentebudget.js");
-/d:/Proyectos/Automatización/GITHUB/svg-server/cuatroese/budgets/index.js
+const componenteBudgetRoute = require("./componenteBudget.js");
+
 // Puedes usar el nombre que quieras en la URL, no afecta
-router.use("/componente", componentebudgetRoute);
+router.use("/componente", componenteBudgetRoute);
 
 module.exports = router;
